c251ff6afad333340b057419889e828597a9888c
mohammadR <mohammadrabetian@gmail.com>
2019-10-05 17:37:21 +0330

    added cancel parking reservation

--- reservation/api/urls.py
+++ reservation/api/urls.py
@@ -3,11 +3,13 @@ from reservation.api.views.reserve import reserve_parking
 from reservation.api.views.available_parkings import available_parkings
 from reservation.api.views.exit_parking import exit_parking
 from reservation.api.views.create_parking import create_parking
+from reservation.api.views.cancel_reserve import cancel_reserve
 
 
 urlpatterns = [
     path('reserve', reserve_parking, name='reserve-parking'),
     path('available_parkings', available_parkings, name='available-parkings'),
     path('exit', exit_parking, name='exit'),
-    path('create', create_parking, name='create-parking')
+    path('create', create_parking, name='create-parking'),
+    path('cancel', cancel_reserve, name='cancel')
 ]
---
+++ reservation/api/views/cancel_reserve.py
@@ -0,0 +1,27 @@
+from django.views.decorators.csrf import csrf_exempt
+import json
+from reservation.models import Reservation
+from django.http import JsonResponse
+
+
+@csrf_exempt
+def cancel_reserve(request):
+    request_body = request.body.decode('utf-8')
+    data = json.loads(request_body)
+
+    parking_slot = data.get('parking_slot_id')
+    current_user = request.user
+    if not current_user.is_authenticated:
+        return JsonResponse({"result": "You must login first!"})
+
+    try:
+        instance = Reservation.objects.filter(
+            user_id=current_user.id, parking_slot_id=parking_slot
+        )
+        if instance:
+            instance.delete()
+            return JsonResponse({"result": "Reservation canceled!"})
+        else:
+            return JsonResponse({"result": "No such reservation exists!"})
+    except Exception:
+        return JsonResponse({"result": "Bad Request"})
