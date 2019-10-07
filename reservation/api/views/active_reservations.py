d8a610c2aef99c62ce2ed4b6330103f3539ef833
mohammadR <mohammadrabetian@gmail.com>
2019-10-07 10:58:29 +0330

    minor change to the values

--- reservation/api/views/active_reservations.py
+++ reservation/api/views/active_reservations.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 def active_reservations(request):
     reservations = Reservation.objects.filter(
         exit_date__isnull=True
-    ).values('id', 'created_date', 'start_date', 'finish_date', 'parking_slot', 'user')
+    ).values('id', 'created_date', 'start_date', 'finish_date', 'parking_slot', 'user__username')
     if reservations:
         return JsonResponse({"result": list(reservations)})
     return JsonResponse({"result": "There are no reservations right now!"})
