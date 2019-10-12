0cb0558f37095a093f22474b0d8f794820764d16
mohammadR <mohammadrabetian@gmail.com>
2019-10-12 13:03:36 +0330

    updating reservation object modified

--- reservation/api/views/helpers/help_reserve.py
+++ reservation/api/views/helpers/help_reserve.py
@@ -1,12 +1,19 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+
 def check_previous_reserve(model: object, data: dict) -> None:
-    previous_slot_reserve = model.objects.filter(
-        parking_slot_id=data.get("parking_slot_id"),
-        start_date=None,
-        finish_date=None,
-        exit_date__isnull=False,
-    )
-    if previous_slot_reserve:
-        data["exit_date"] = None
-        previous_slot_reserve.update(**data)
+    try:
+        previous_slot_reserve = model.objects.get(
+            parking_slot_id=data.get("parking_slot_id"),
+            start_date=None,
+            finish_date=None,
+            exit_date__isnull=False,
+        )
+        previous_slot_reserve.start_date = data.get("start_date")
+        previous_slot_reserve.finish_date = data.get("finish_date")
+        previous_slot_reserve.exit_date = None
+        previous_slot_reserve.save()
         return
+    except ObjectDoesNotExist:
+        pass
     model(**data).save()
