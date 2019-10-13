4157696e596bb0d84f624a485ccb6d4dc60d86da
mohammadR <mohammadrabetian@gmail.com>
2019-10-13 11:43:27 +0330

    user cannot enter parking if finish time has passed or start time hasn't arrived yet

--- reservation/api/views/cancel_reserve.py
+++ reservation/api/views/cancel_reserve.py
@@ -4,7 +4,7 @@ from reservation.models import Reservation
 from django.http import JsonResponse
 from reservation.decorators.validate_params import validate_params
 
-schema = {"parking_slot_id": {"type": "integer", "required": True}}
+schema = {"id": {"type": "integer", "required": True}}
 
 
 @csrf_exempt
@@ -13,14 +13,14 @@ def cancel_reserve(request):
     request_body = request.body.decode("utf-8")
     data = json.loads(request_body)
 
-    parking_slot = data.get("parking_slot_id")
+    reservation = data.get("id")
     current_user = request.user
     if not current_user.is_authenticated:
         return JsonResponse({"result": "You must login first!"})
 
     try:
         instance = Reservation.objects.filter(
-            user_id=current_user.id, parking_slot_id=parking_slot
+            user_id=current_user.id, id=reservation
         ).first()
         if instance:
             if instance.enter_date:
--- reservation/api/views/enter_parking.py
+++ reservation/api/views/enter_parking.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from reservation.decorators.validate_params import validate_params
 
 
-schema = {"parking_slot_id": {"type": "integer", "required": True}}
+schema = {"id": {"type": "integer", "required": True}}
 
 
 @csrf_exempt
@@ -17,13 +17,14 @@ def enter_parking(request):
     current_user = request.user
     # you cannot enter the parking slot if the slot isn't freed yet
     if Reservation.objects.filter(
-        parking_slot_id=data.get("parking_slot_id"),
-        enter_date__isnull=False,
-        finish_date__gt=timezone.now(),
+        id=data.get("id"), enter_date__isnull=False, finish_date__gt=timezone.now()
     ).exists():
         return JsonResponse({"result": "This Parking is still occupied!"})
     reserved_parking = Reservation.objects.filter(
-        user_id=current_user.id, parking_slot_id=data.get("parking_slot_id")
+        user_id=current_user.id,
+        id=data.get("id"),
+        finish_date__gt=timezone.now(),
+        start_date__lte=timezone.now(),
     )
     if reserved_parking:
         reserved_parking.update(enter_date=timezone.now(), exit_date=None)
