e9858a498299626d0e5bf89d26cd64bcc51caa35
MohammadR <mohammadrabetian@gmail.com>
2019-10-14 23:10:49 +0330

    made changes to reservation saving model for test

--- reservation/api/views/helpers/help_reserve.py
+++ reservation/api/views/helpers/help_reserve.py
@@ -1,17 +1,17 @@
-import contextlib
+# import contextlib
 
 
 def check_previous_reserve(model: object, data: dict) -> None:
-    with contextlib.suppress(Exception):
-        previous_slot_reserve = model.objects.get(
-            parking_slot_id=data.get("parking_slot_id"),
-            start_date=None,
-            finish_date=None,
-            exit_date__isnull=False,
-        )
-        previous_slot_reserve.start_date = data.get("start_date")
-        previous_slot_reserve.finish_date = data.get("finish_date")
-        previous_slot_reserve.exit_date = None
-        previous_slot_reserve.save()
-        return
+    # with contextlib.suppress(Exception):
+    #     previous_slot_reserve = model.objects.get(
+    #         parking_slot_id=data.get("parking_slot_id"),
+    #         start_date=None,
+    #         finish_date=None,
+    #         exit_date__isnull=False,
+    #     )
+    #     previous_slot_reserve.start_date = data.get("start_date")
+    #     previous_slot_reserve.finish_date = data.get("finish_date")
+    #     previous_slot_reserve.exit_date = None
+    #     previous_slot_reserve.save()
+    #     return
     model(**data).save()
