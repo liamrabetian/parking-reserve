c01738419872437c029455050f8d6a42b1b76e57
Naapi <terhocraven@gmail.com>
2019-10-28 13:07:44 +0200

    added tests for parking models and removed requirement pkg-resources as pip doesnt find it

--- parking/tests.py
+++ parking/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from parking.models import Floor, ParkingSlot
 
 # Create your tests here.
+#SQL Lite doesnt care about length https://sqlite.org/faq.html#q9
+#MySQL or Postgresql would work
+class FloorTestCase(TestCase):
+    """Create floor object"""
+    def create_floor(self, floor_number="2A"):
+        return Floor.objects.create(floor_number=floor_number)
+    """Test floor model"""
+    def test_create_floor(self):
+        testFloor = self.create_floor()
+        self.assertTrue(isinstance(testFloor, Floor))
+        self.assertEqual(testFloor.__str__(), testFloor.floor_number)
+
+class ParkingSlotTestCase(TestCase):
+    def create_parkingslot(self):
+        #Create test object
+        floor = Floor(floor_number="2A")
+        floor.save()
+        slot = ParkingSlot(slot_number="11")
+        slot.save()
+        #Test if ParkingSlot model foreignkey is linked to Floor model
+        test_parking = ParkingSlot.objects.get(id=1)
+        self.assertEqual(test_parking.floor.floor_number, "2A")
