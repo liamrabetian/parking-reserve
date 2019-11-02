8c21f818320b8bd61a30b4ec550a97d2c8be6148
Mohammad Rabetian <tommyrabetian@gmail.com>
2019-11-02 23:09:34 +0330

    added login required decorator to logout api

--- users/api/views/user_logout.py
+++ users/api/views/user_logout.py
@@ -2,14 +2,12 @@ from django.contrib.auth import logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from reservation.decorators.request_method import check_request_method
+from reservation.decorators.login_required import login_required
 
 
 @csrf_exempt
+@login_required
 @check_request_method(method="GET")
 def user_logout(request):
-    current_user = request.user
-    if current_user.is_authenticated:
-        logout(request)
-        return JsonResponse({"result": "You are now logged out!"}, status=200)
-    else:
-        return JsonResponse({"result": "You aren't logged in!"}, status=400)
+    logout(request)
+    return JsonResponse({"result": "You are now logged out!"}, status=200)
